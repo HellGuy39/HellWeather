6bfb1d91e5e615bb403dd1bc66e0042cfa2db602
HellGuy39 <alexey2005yt@gmail.com>
2021-12-26 18:25:24 +0300

    1.0 alpha2-02

--- app/src/main/java/com/hellguy39/hellweather/HomeFragment.kt
+++ app/src/main/java/com/hellguy39/hellweather/HomeFragment.kt
@@ -11,17 +11,25 @@ import com.hellguy39.hellweather.utils.OW_API_KEY
 import com.hellguy39.hellweather.utils.WeatherModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.Dispatchers.Main
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import okhttp3.*
+import okhttp3.internal.wait
 import org.json.JSONObject
 import java.io.IOException
 import java.net.URL
+import java.text.SimpleDateFormat
+import java.util.*
 
 class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
 
-    var okHttpClient: OkHttpClient = OkHttpClient()
+    private var okHttpClient: OkHttpClient = OkHttpClient()
+    private val city = "Krasnodar"
+    private val url = "https://api.openweathermap.org/data/2.5/weather?q=$city&units=metric&appid=$OW_API_KEY"
+    private val LOG_HOME_FRAGMENT = "HomeFragment"
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,6 +43,7 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding = FragmentHomeBinding.bind(view)
+        binding.rootView.setBackgroundResource(R.drawable.gradient_day)
         binding.rootView.setOnRefreshListener {
             onRefresh()
         }
@@ -48,49 +57,70 @@ class HomeFragment : Fragment() {
 
     private fun onRefresh() {
         CoroutineScope(IO).launch {
-            requestAPI()
-            updateUI()
+            val request: Request = Request
+                .Builder()
+                .url(url)
+                .build()
+            request(request)
         }
     }
 
-    private suspend fun updateUI() {
-
+    private fun updateUI(wm : WeatherModel) {
+        CoroutineScope(Main).launch {
+            Log.i(LOG_HOME_FRAGMENT, "updateUI() was called")
+            binding.rootView.isRefreshing = false
+            binding.tvTemp.text = wm.temp
+            binding.tvMaxTemp.text = wm.tempMax
+            binding.tvMinTemp.text = wm.tempMin
+            binding.tvUpdateTime.text = wm.updateTime
+            binding.tvWeather.text = wm.wDescription
+            binding.tvCity.text = wm.city
+            //binding.tvUpdateTime.text = wm.updateTime
+        }
     }
 
-    private suspend fun requestAPI() {
-        val city = "Krasnodar"
-        val url = "https://api.openweathermap.org/data/2.5/weather?q=$city&units=metric&appid=$OW_API_KEY"
-        val request: Request = Request
-            .Builder()
-            .url(url)
-            .build()
+    private fun request(request: Request){
+        Log.i(LOG_HOME_FRAGMENT, "request() was called")
+        val wm = WeatherModel()
 
         okHttpClient
             .newCall(request)
             .enqueue(object: Callback {
-            override fun onFailure(call: Call, e: IOException)
-            {
-
-            }
-
-            override fun onResponse(call: Call, response: Response)
-            {
-                //val wm = WeatherModel()
-
-                if (response.isSuccessful)
-                {
-                    val result = response.body?.string()
-                    result?.let { Log.i("LOG", it) }
-                    val jsonObject = JSONObject(result)
-                    val main = jsonObject.getJSONObject("main")
-                    val temp = main.getString("temp")
-                    Log.i("LOG_TAG", temp)
-                }
-                else
+                override fun onFailure(call: Call, e: IOException)
                 {
 
                 }
-            }
-        })
+
+                override fun onResponse(call: Call, response: Response)
+                {
+
+                    if (response.isSuccessful)
+                    {
+                        val result = response.body?.string()
+                        result?.let { Log.i("LOG", it) }
+                        val jsonObject = JSONObject(result)
+                        val main = jsonObject.getJSONObject("main")
+                        wm.temp = main.getInt("temp").toString() + "°"
+                        wm.tempFeelsLike = main.getString("feels_like")
+                        wm.tempMax = main.getInt("temp_max").toString() + "° C"
+                        wm.tempMin = main.getInt("temp_min").toString() + "° C"
+
+                        val time: Long = jsonObject.getLong("dt")
+
+                        wm.updateTime = SimpleDateFormat("E, HH:mm", Locale.ENGLISH).format(Date(time * 1000))
+                        wm.city = city
+
+                        val weather = jsonObject.getJSONArray("weather").getJSONObject(0)
+                        wm.wDescription = weather.getString("description")
+                            .replaceFirstChar { if (it.isLowerCase()) it.titlecase(Locale.getDefault()) else it.toString() }
+                        updateUI(wm)
+                        //wm.updateTime =
+                    }
+                    else
+                    {
+
+                    }
+                }
+            })
     }
 }
--- app/src/main/java/com/hellguy39/hellweather/utils/WeatherModel.kt
+++ app/src/main/java/com/hellguy39/hellweather/utils/WeatherModel.kt
@@ -1,5 +1,13 @@
 package com.hellguy39.hellweather.utils
 
 data class WeatherModel(
-    var temp: String = "none"
+    var name: String = "",
+    var city: String = "",
+    var temp: String = "",
+    var tempMax: String = "",
+    var tempMin: String = "",
+    var tempFeelsLike: String = "",
+    var wind: String = "",
+    var updateTime: String = "",
+    var wDescription: String = ""
 )
