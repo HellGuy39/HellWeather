38b0848f83f21689779d52edca3c7c06e0a94ec6
Aleksey Gadzhiev <alexey2005yt@gmail.com>
2022-01-10 23:25:22 +0300

    1.0 alpha3-10

--- app/src/main/java/com/hellguy39/hellweather/presentation/fragments/location_manager/LocationManagerViewModel.kt
+++ app/src/main/java/com/hellguy39/hellweather/presentation/fragments/location_manager/LocationManagerViewModel.kt
@@ -1,7 +1,35 @@
 package com.hellguy39.hellweather.presentation.fragments.location_manager
 
 import androidx.lifecycle.ViewModel
+import com.hellguy39.hellweather.repository.database.LocationRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-class LocationManagerViewModel : ViewModel() {
+@HiltViewModel
+class LocationManagerViewModel @Inject constructor(
+    private val repository: LocationRepository
+): ViewModel() {
+
+    val locations = repository.getLocations()
+
+    fun onEvent(event: LocationListEvent) {
+        when(event) {
+            is LocationListEvent.OnDeleteLocation -> {
+
+            }
+            is LocationListEvent.OnAddItemClick -> {
+
+            }
+            is LocationListEvent.OnDoneChange -> {
+
+            }
+            is LocationListEvent.OnItemClick -> {
+
+            }
+            is LocationListEvent.OnUndoDeleteClick -> {
+
+            }
+        }
+    }
 
 }
